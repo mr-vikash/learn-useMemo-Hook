a327f30cb0e313a227f3d5587b56ba01bcef66e7
mr-vikash <mrvikash700@gmail.com>
2023-08-26 23:22:07 +0530

    learn usememo-hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState,useMemo } from 'react';
+import './App.css';
+import Header from './components/Header';
+
+const  App =()=> {
+  const [count,setCount] = useState(0);
+  const [name,setName] = useState('');
+
+  const expensiveCalculation = (num)=>{
+    for(var i = 0;i<=1000000000;i++){}
+    return num;
+  }
+
+  const calculation = useMemo(()=>{
+    expensiveCalculation(count);
+  },[count])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <button onClick={()=>setCount(count + 1)}>Increment</button>
+      <h1>Count:{count}</h1>
+      <input type='text' name='name' onChange={(e)=>setName(e.target.value)}/>
+      <h1>Name:{name}</h1>
     </div>
   );
 }
